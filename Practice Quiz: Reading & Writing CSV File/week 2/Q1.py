8b80e68c34802cd71dc2ef946e8f67e13956172b
Jeevan V Jijo <67267390+jeevan-v-jijo@users.noreply.github.com>
2022-12-07 22:20:28 +0530

    Update Q1.py

--- Practice Quiz: Reading & Writing CSV File/week 2/Q1.py
+++ Practice Quiz: Reading & Writing CSV File/week 2/Q1.py
@@ -29,3 +29,10 @@ def contents_of_file(filename):
 
 # Call the function
 print(contents_of_file("flowers.csv"))
+
+# Output:
+# a pink carnation is annual
+# a yellow daffodil is perennial
+# a blue iris is perennial
+# a red poinsettia is perennial
+# a yellow sunflower is annual
