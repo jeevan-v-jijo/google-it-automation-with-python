ded258cf02c84ca20e3475b0bd8498b8ca457313
Jeevan V Jijo <67267390+jeevan-v-jijo@users.noreply.github.com>
2022-12-08 15:19:36 +0530

    Update Q1.py

--- Practice Quiz: Reading & Writing CSV File/week 2/Q1.py
+++ Practice Quiz: Reading & Writing CSV File/week 2/Q1.py
@@ -1,3 +1,9 @@
+'''
+Question 1
+We're working with a list of flowers and some information about each one. The create_file function writes this information to a CSV file.
+The contents_of_file function reads this file into records and returns the information in a nicely formatted block.
+Fill in the gaps of the contents_of_file function to turn the data in the CSV file into a dictionary using DictReader.
+'''
 import os
 import csv
 
