ff2d09bc0314ddcc6acd8e0e9b2cf8fa6c7d9187
Jeevan V Jijo <67267390+jeevan-v-jijo@users.noreply.github.com>
2022-12-21 19:00:25 +0530

    Create repition.py

---
+++ Course2/week3/in_video_questions/repition.py
@@ -0,0 +1,21 @@
+#video:more on repitition
+#The long_words function returns all words that are at least 7 characters. Fill in the regular expression to complete this function.
+
+import re
+def long_words(text):
+  pattern ="\w{7,}"
+  result = re.findall(pattern, text)
+  return result
+
+print(long_words("I like to drink coffee in the morning.")) # ['morning']
+print(long_words("I also have a taste for hot chocolate in the afternoon.")) # ['chocolate', 'afternoon']
+print(long_words("I never drink tea late at night.")) # []
+
+
+# Here is your output:
+# ['morning']
+# ['chocolate', 'afternoon']
+# []
+
+# Nice job! Your regular expressions are getting more and more
+# sophisticated!
