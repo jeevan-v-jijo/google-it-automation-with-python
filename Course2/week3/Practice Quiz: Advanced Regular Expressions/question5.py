f18ceb163f5da02e2d45feb7b1db54a640f148a0
Jeevan V Jijo <67267390+jeevan-v-jijo@users.noreply.github.com>
2022-12-29 21:48:56 +0530

    Update question5.py
    a dot was missing in the regex

--- Course2/week3/Practice Quiz: Advanced Regular Expressions/question5.py
+++ Course2/week3/Practice Quiz: Advanced Regular Expressions/question5.py
@@ -4,7 +4,7 @@
 
 import re
 def convert_phone_number(phone):
-  result = re.sub("(\d{3})-(\d{3})-(\d{3})",r"(\1) \2 \3",phone)
+  result = re.sub(".(\d{3})-(\d{3})-(\d{3})",r"(\1) \2 \3",phone)
   return result
 
 print(convert_phone_number("My number is 212-345-9999.")) # My number is (212) 345-9999.
@@ -13,7 +13,7 @@ print(convert_phone_number("123-123-12345")) # 123-123-12345
 print(convert_phone_number("Phone number of Buckingham Palace is +44 303 123 7300")) # Phone number of Buckingham Palace is +44 303 123 7300
 
 #this is the output
-# My number is (212) 345 9999.
-# Please call (888) 555 1234
-# (123) 123 12345
+# My number is(212) 345 9999.
+# Please call(888) 555 1234
+# 123-123-12345
 # Phone number of Buckingham Palace is +44 303 123 7300
